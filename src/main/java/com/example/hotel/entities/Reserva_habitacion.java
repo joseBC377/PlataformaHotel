636bb1c50eb0e53a53e799e57dd44f7d259ea0b9
Daniela <danielasf2002@outlook.es>
2025-04-19 19:56:54 -0500

    en la entidad reserva activar el atributo habitacion cuando exista su entidad

---
+++ src/main/java/com/example/hotel/entities/Reserva_habitacion.java
@@ -0,0 +1,44 @@
+package com.example.hotel.entities;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "RESERVA_HABITACION")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Reserva_habitacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @NotNull(message = "El precio es obligatorio")
+    @PositiveOrZero(message = "El precio debe ser un valor positivo")
+    @Column(name = "Precio", precision = 10, scale = 2)
+    private BigDecimal precio;
+
+    @ManyToOne
+    @JoinColumn(name = "id_reserva", nullable = true)
+    private Reserva reserva;
+
+//------activar habitacion cuando se cree sus entidad
+    // @ManyToOne
+    // @JoinColumn(name = "id_habitacion", nullable = true)
+    // private Habitacion habitacion;
+
+}
