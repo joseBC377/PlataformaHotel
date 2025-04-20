b6e456fbcbdb8bb272c7221417cbba1f2b7e9bc0
Daniela <danielasf2002@outlook.es>
2025-04-19 20:12:02 -0500

    reserva habitacion repository listo, pero activar el findby de habitacion

---
+++ src/main/java/com/example/hotel/repository/Reserva_habitacionRepository.java
@@ -0,0 +1,13 @@
+package com.example.hotel.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.hotel.entities.Reserva_habitacion;
+
+public interface Reserva_habitacionRepository extends JpaRepository<Reserva_habitacion,Integer>{
+    List<Reserva_habitacion> findByReservaId(Integer ReservaID);
+    // activar cuando habitacion exista
+    // List<Reserva_habitacion> findByHabitacionId(Integer HabitacionID);
+}
