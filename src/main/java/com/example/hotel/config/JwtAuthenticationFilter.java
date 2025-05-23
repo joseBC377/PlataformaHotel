aec12b1200bdafb7ea27ffec31f3211527696383
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-05-22 23:58:21 -0500

    JWT 65 porciento

--- src/main/java/com/example/hotel/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/hotel/config/JwtAuthenticationFilter.java
@@ -1,13 +1,40 @@
 package com.example.hotel.config;
 
-import org.springframework.stereotype.Component;
+import java.io.IOException;
 
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.hotel.services.JwtService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    
+    
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+            )
+            throws ServletException, IOException {
+
+    }
 
     
+
 
 }
