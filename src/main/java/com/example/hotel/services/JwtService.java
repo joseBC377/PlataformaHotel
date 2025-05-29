c727e0943de34fe9a055b0927a270d3a41809947
JoseBC <jbalcazar377@gmail.com>
2025-05-28 21:39:13 -0500

    Limpiar import vacios y colocar fetch

--- src/main/java/com/example/hotel/services/JwtService.java
+++ src/main/java/com/example/hotel/services/JwtService.java
@@ -4,11 +4,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import javax.crypto.SecretKey;
 
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
