82287d5453e00aafe833dc3aea99dacc063b12de
JoseBC <jbalcazar377@gmail.com>
2025-07-03 19:54:22 -0500

    se cambiola jerarquia de requestmatcher

--- src/main/java/com/example/hotel/HotelApplication.java
+++ src/main/java/com/example/hotel/HotelApplication.java
@@ -24,11 +24,11 @@ public class HotelApplication {
 			System.out.println("Creando usuario");
 			if (usuarioRepository.findByCorreo("durand@gmail.com").isEmpty()) { //Ponemos la condicional para ver si exite  no lo crea , pero si no exite lo crea en la databse
 				Usuario usuario = new Usuario();
-				usuario.setTelefono("123456789");
+				usuario.setTelefono("123456");
 				usuario.setApellido("durand");
 				usuario.setNombre("durand");
 				usuario.setCorreo("durand@gmail.com");
-				usuario.setPassword(encoder.encode("123456"));
+				usuario.setPassword(encoder.encode("12345678"));
 				usuario.setRol(Rol.ADMIN);
 				usuarioRepository.save(usuario);
 			}
--- src/main/java/com/example/hotel/config/SecurityConfig.java
+++ src/main/java/com/example/hotel/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.hotel.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,7 +13,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,15 +23,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-
     private final JwtAuthenticationFilter jwtAuthFilter;
 
-
     @Bean
-    public AuthenticationManager authenticationManager (AuthenticationConfiguration configuration) throws Exception{
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
-
 
     // encripta la contraseña ingresada para ser comparada con la de la database
     @Bean
@@ -37,14 +37,17 @@ public class SecurityConfig {
     }
 
     // Esta es la parte importante donde hara el proceso de autenticacion
-    /*@Bean
-    DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(service); // Obtiene al usuario
-        provider.setPasswordEncoder(passwordEncoder()); // Compara cotraseñas codificadas
-        return provider;
-
-    }*/
+    /*
+     * @Bean
+     * DaoAuthenticationProvider authenticationProvider() {
+     * DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+     * provider.setUserDetailsService(service); // Obtiene al usuario
+     * provider.setPasswordEncoder(passwordEncoder()); // Compara cotraseñas
+     * codificadas
+     * return provider;
+     * 
+     * }
+     */
 
     // @Bean
     // SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
@@ -54,27 +57,41 @@ public class SecurityConfig {
     // .requestMatchers("/api/usuario/privado").hasAnyAuthority("ADMIN") //te pide
     // //autenticacion y para que ingreses debes de tener como rol ADMIN
     // .anyRequest().permitAll()) //Cualquier otra ruta tambien libre
-    // .httpBasic(Customizer.withDefaults()) //POR DEFECTO EN EL NAVEGADO SALE UN LOGIN BASICO
+    // .httpBasic(Customizer.withDefaults()) //POR DEFECTO EN EL NAVEGADO SALE UN
+    // LOGIN BASICO
     // .build();
-    //}
+    // }
 
-    //.formLogin(Customizer.withDefaults())
-    //.build();
-    
+    // .formLogin(Customizer.withDefaults())
+    // .build();
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/api/v1/auth/**").permitAll() //Ingresar sin token
-            .requestMatchers("/api/usuario/publico","/api/usuario/**").hasAuthority("CLIENT")
-            .requestMatchers("/api/usuario/privado").hasAuthority("ADMIN")
-            //Cualquier otra ruta no mencionada necesita que el usuario sea autenticado pero sin importar si es admin o client
-            .anyRequest().authenticated())
-        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);            
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/**").permitAll() // Ingresar sin token
+                        .requestMatchers("api/usuario/todosCliente").hasAuthority("ADMIN")
+                        .requestMatchers("/api/usuario/publico", "/api/usuario/**").hasAuthority("CLIENT")
+                        // Cualquier otra ruta no mencionada necesita que el usuario sea autenticado
+                        // pero sin importar si es admin o client
+                        .anyRequest().authenticated())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(
+                Arrays.asList("http://127.0.0.1:3000", "http://localhost:4200", "http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
