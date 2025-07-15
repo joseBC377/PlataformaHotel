f37ab2a920c44cba45d464209b1878d924610355
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-07-14 23:09:36 -0500

    Crud pulido

--- src/main/java/com/example/hotel/controllers/UsuarioRestController.java
+++ src/main/java/com/example/hotel/controllers/UsuarioRestController.java
@@ -12,7 +12,6 @@ import lombok.AllArgsConstructor;
 import java.util.List;
 
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
